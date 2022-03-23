c552e88e827949a5eec06be51fae750513266842
Venkatesh N S <85620459+NSVenkatesh@users.noreply.github.com>
2022-03-23 23:04:30 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
+let data = [
+  {
+    img: "img/flipkart.png",
+    name: "Flipkart mobile listing prototype",
+    link: "https://nsvenkatesh.github.io/flipkart/Flipkart/",
+  },
+  {
+    img: "img/2048game.png",
+    name: "2048 game",
+    link: "https://nsvenkatesh.github.io/2048game/2048/",
+  },
+  {
+    img: "img/currencyconvertor.png",
+    name: "Currency convertor",
+    link: "http://exchangerate.orgfree.com/Exchangerate/index.html",
+  },
+  {
+    img: "img/sample-post.png",
+    name: "Sample post with gif",
+    link: "https://nsvenkatesh.github.io/sample-post/",
+  },
+];
+
 $(document).ready(function () {
   let x = 0;
   window.history.pushState("", "", "#home");
@@ -49,5 +72,24 @@ $(document).ready(function () {
       $(".side-nav").hide(600);
       $(".checkbox").prop("checked", false);
     }
+  }
+  for (i in data) {
+    let img = data[i].img;
+    let name = data[i].name;
+    let link = data[i].link;
+    $(".p-flex").append(`<div class="p-content">
+      <div class="img-scale">
+        <img src=${img} alt=${name} />
+      </div>
+      <p>${name}</p>
+      <a
+        href=${link}
+        target="_blank"
+        title=${name}
+        rel="noopener"
+      >
+        <button class="p-btn1">Live preview</button>
+      </a>
+    </div>`);
   }
 });
